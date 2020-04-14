c38a23c9d115778e08151646170e0134cbb913d0
bruce willis <fashun_vyctum@yahoo.com>
2020-04-14 02:25:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,22 +20,22 @@ class TicTacToe
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+  def input_to_index(user_input)
+   user_input.to_i - 1
+  end
 
-def move(index, current_player = "X")
-  @board[index] = current_player
-end
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
 
-def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
-end
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
 
-def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+  def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index)
 end
 
 def turn_count
@@ -48,50 +48,51 @@ def turn_count
   return turn
 end
 
-def current_player
-  num_turns = turn_count
-  if num_turns % 2 == 0
-    player = "X"
-  else
-    player = "O"
+  def current_player
+   num_turns = turn_count
+   if num_turns % 2 == 0
+     player = "X"
+   else
+     player = "O"
+   end
+    return player
   end
-  return player
-end
 
-def turn
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(index)
-    player_token = current_player
-    move(index, player_token)
-    display_board
-  else
-    turn
-  end
-end
-  
-def won?
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-
-    position_1 = @board[index_0]
-    position_2 = @board[index_1]
-    position_3 = @board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+  def turn
+    puts "Please choose a number 1-9:"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      player_token = current_player
+     move(index, player_token)
+      display_board
+    else
+     turn
     end
-  }
-  return false
-end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.each {|win_combo|
+      index_0 = win_combo[0]
+      index_1 = win_combo[1]
+      index_2 = win_combo[2]
 
-def full?
-  @board.all? {|index| index == "X" || index == "O"}
-end
+      position_1 = @board[index_0]
+      position_2 = @board[index_1]
+      position_3 = @board[index_2]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
+     end
+   }
+    return false
+  end
+
+  def full?
+    @board.all? {|index| index == "X" || index == "O"}
+  end
+
   
 end
